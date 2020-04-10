e273cb5d58b8d7f927117b6bc0945482eb4dd6ab
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-10 18:44:57 +0200

    created  dbschema file to show the data structure for db

---
+++ socialape-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2020-04-10T16:44:03.339Z',
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+}
