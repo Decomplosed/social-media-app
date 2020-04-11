3af7f15d19be659656781f8369c03332acd378db
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-11 13:25:42 +0200

    imported fbAuth middleware function

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions')
 const app = require('express')()
 
+const FBAuth = require('./utils/fbAuth')
+
 const { getAllScreams, postOneScream } = require('./handlers/screams')
 const { signup, login } = require('./handlers/users')
 
