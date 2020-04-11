2d4cc68ab9d5efcec8dc639189c00c0087525460
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-11 13:13:56 +0200

    moved login route handler to the users file

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -67,3 +67,33 @@ exports.signup = (req, res) => {
       }
     })
 }
+
+exports.login = (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  }
+
+  let errors = {}
+
+  if (isEmpty(user.email)) errors.email = 'Must not be empty'
+  if (isEmpty(user.password)) errors.password = 'Must not be empty'
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken()
+    })
+    .then((token) => res.json({ token }))
+    .catch((err) => {
+      console.error(err)
+      if (err.code === 'auth/wrong-password') {
+        return res
+          .status(403)
+          .json({ general: 'Wrong credentials, please try again' })
+      } else return res.status(500).json({ error: error.code })
+    })
+}
