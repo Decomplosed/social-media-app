79388c90b3f4f2a43b405dedc8d5e493522ba8d1
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-11 13:25:31 +0200

    if valid that proceed with signup

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -65,12 +65,9 @@ exports.login = (req, res) => {
     password: req.body.password,
   }
 
-  let errors = {}
+  const { valid, errors } = validateLoginData(user)
 
-  if (isEmpty(user.email)) errors.email = 'Must not be empty'
-  if (isEmpty(user.password)) errors.password = 'Must not be empty'
-
-  if (Object.keys(errors).length > 0) return res.status(400).json(errors)
+  if (!valid) return res.status(400).json(errors)
 
   firebase
     .auth()
