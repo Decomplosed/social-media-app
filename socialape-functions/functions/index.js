aaaf69e3101beed076aabe57f241966e8f1b02af
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-11 10:42:11 +0200

    added condition to check email validation

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -57,7 +57,7 @@ app.post('/scream', (req, res) => {
     })
 })
 
-const isEmail = email => {
+const isEmail = (email) => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   if (email.match(regEx)) return true
   else return false
@@ -79,7 +79,9 @@ app.post('/signup', (req, res) => {
   let errors = {}
 
   if (isEmpty(newUser.email)) {
-    error.email = 'Email must not be empty'
+    errors.email = 'Email must not be empty'
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address'
   }
 
   // TODO Validate Data
