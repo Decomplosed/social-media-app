2b18898fdf7acb3b7a400fa330a8eda98f973c53
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-11 13:11:12 +0200

    moved signup to users handlers

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -3,15 +3,6 @@ const app = require('express')()
 
 const { getAllScreams, postOneScream } = require('./handlers/screams')
 
-const config = {
-  apiKey: 'AIzaSyBTdQUqGlP-dk4vmxuiSBMYNPXgnUt05UQ',
-  authDomain: 'socialape-43478.firebaseapp.com',
-  databaseURL: 'https://socialape-43478.firebaseio.com',
-  projectId: 'socialape-43478',
-  storageBucket: 'socialape-43478.appspot.com',
-  messagingSenderId: '38737443979',
-  appId: '1:38737443979:web:cf44f65b6140eaec7d288e',
-}
 
 const firebase = require('firebase')
 
@@ -66,71 +57,7 @@ const isEmpty = (string) => {
   else return false
 }
 
-app.post('/signup', (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle,
-  }
-
-  let errors = {}
-
-  if (isEmpty(newUser.email)) {
-    errors.email = 'Must not be empty'
-  } else if (!isEmail(newUser.email)) {
-    errors.email = 'Must be a valid email address'
-  }
-
-  if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
-  if (newUser.password !== newUser.confirmPassword)
-    errors.confirmPassword = 'Passwords must be the same'
-  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
-
-  if (Object.keys(errors).length > 0) return res.status(400).json(errors)
-
-  // TODO Validate Data
-
-  let token, userId
-
-  db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        return res.status(400).json({ handle: 'This handle is already taken' })
-      } else {
-        return firebase
-          .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password)
-      }
-    })
-    .then((data) => {
-      userId = data.user.uid
-      return data.user.getIdToken()
-    })
-    .then((idToken) => {
-      token = idToken
-      const userCredentials = {
-        handle: newUser.handle,
-        email: newUser.email,
-        createdAt: new Date().toISOString(),
-        userId,
-      }
-
-      db.doc(`/users/${newUser.handle}`).set(userCredentials)
-    })
-    .then(() => {
-      return res.status(201).json({ token })
-    })
-    .catch((err) => {
-      console.error(err)
-      if (err.code === 'auth/email-already-in-use') {
-        return res.status(400).json({ email: 'Email is already in use' })
-      } else {
-        return res.status(500).json({ error: error.code })
-      }
-    })
-})
+app.post('/signup', signup)
 
 app.post('/login', (req, res) => {
   const user = {
