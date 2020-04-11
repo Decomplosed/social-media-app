9b82c0bdcf73b21c5edb008b040e889dd50fe16e
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-11 13:10:48 +0200

    moved config to separated file

---
+++ socialape-functions/functions/utils/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyBTdQUqGlP-dk4vmxuiSBMYNPXgnUt05UQ',
+  authDomain: 'socialape-43478.firebaseapp.com',
+  databaseURL: 'https://socialape-43478.firebaseio.com',
+  projectId: 'socialape-43478',
+  storageBucket: 'socialape-43478.appspot.com',
+  messagingSenderId: '38737443979',
+  appId: '1:38737443979:web:cf44f65b6140eaec7d288e',
+}
