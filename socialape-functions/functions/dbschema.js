50499b1ed7b33d7b24f8b6622fde6c81763a0ff5
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-12 17:56:15 +0200

    updated dbschema with users example

--- socialape-functions/functions/dbschema.js
+++ socialape-functions/functions/dbschema.js
@@ -1,4 +1,17 @@
 let db = {
+  users: [
+    {
+      userId: 'CxTCJist2fANudS0ds0I',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2020-04-11T10:37:25.013Z',
+      imageUrl:
+        'https://firebasestorage.googleapis.com/v0/b/socialape-43478.appspot.com/o/93727243.png?alt=media',
+      bio: 'Hello, my name is user',
+      website: 'https://www.user.com',
+      location: 'London, UK',
+    },
+  ],
   screams: [
     {
       userHandle: 'user',
