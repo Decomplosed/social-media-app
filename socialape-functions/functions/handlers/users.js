2b461b1a43127de4bd5977805084963dfcd36ec5
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-12 09:01:03 +0200

    created uploadImage method

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -91,4 +91,36 @@ exports.uploadImage = (req, res) => {
   const path = require('path')
   const os = require('os')
   const fs = require('fs')
+
+  const busboy = new busboy({ headers: req.headers })
+
+  let imageFileName
+  let imageToBeUploaded = {}
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname)
+    console.log(filename)
+    console.log(mimetype)
+
+    const imageExtension = filename.split('.')[filename.split('.').length - 1]
+    const imageFileName = `Math.round(Math.random() * 1000000).${imageExtension}`
+    const filePath = path.join(os.tmpdir(), imageFileName)
+    imageToBeUploaded = { filePath, mimetype }
+
+    file.pipe(fs.createWriteStream(filePath))
+  })
+
+  busboy.on('finish', () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filePath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype,
+          },
+        },
+      })
+  })
 }
