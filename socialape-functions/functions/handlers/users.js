617ee03670df930ee9b8d4ae36db9817991c1b4d
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-13 13:05:21 +0200

    fixed cpuple of typos

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -110,7 +110,7 @@ exports.addUserDetails = (req, res) => {
 exports.getUserDetails = (req, res) => {
   let userData = {}
 
-  db.doc(`users/${req.params.handle}`)
+  db.doc(`/users/${req.params.handle}`)
     .get()
     .then((doc) => {
       if (doc.exists) {
@@ -119,6 +119,7 @@ exports.getUserDetails = (req, res) => {
           .collection('screams')
           .where('userHandle', '==', req.params.handle)
           .orderBy('createdAt', 'desc')
+          .get()
       } else {
         return res.status(404).json({ error: 'User not found' })
       }
