0ccb660b81d3851d2edccee0d4e0f9ed04ace785
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-13 13:33:10 +0200

    fixed typo inside notifications route

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -40,7 +40,7 @@ app.post('/user/image', FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
 app.get('/user', FBAuth, getAuthenticatedUser)
 app.get('/user/:handle', getUserDetails)
-app.post('/notifictions', FBAuth, markNotificationsRead)
+app.post('/notifications', FBAuth, markNotificationsRead)
 
 exports.api = functions.https.onRequest(app)
 
