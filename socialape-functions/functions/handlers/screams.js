0ac1d0dc250a53f4d888d7248cfd4b3572450f75
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-13 09:50:07 +0200

    created deleteScream method

--- socialape-functions/functions/handlers/screams.js
+++ socialape-functions/functions/handlers/screams.js
@@ -199,3 +199,27 @@ exports.unlikeScream = (req, res) => {
       res.status(500).json({ error: err.code })
     })
 }
+
+exports.deleteScream = (req, res) => {
+  const document = db.doc(`/scream/${req.params.screamId}`)
+
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'Scream not found' })
+      }
+      if (doc.data().userHandle !== req.user.handle) {
+        return res.status(403).json({ error: 'Unauthorized' })
+      } else {
+        return document.delete()
+      }
+    })
+    .then(() => {
+      res.json({ message: 'Scream deleted successfully' })
+    })
+    .catch((err) => {
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
+}
