73e15351c2f296f676cf7cb0e77ac14fc39a0632
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-13 10:41:35 +0200

    created deleteNotificationOnUnlike method

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -72,6 +72,21 @@ exports.createNotificationOnLike = functions
       })
   })
 
+  exports.deleteNotificationOnUnlike = functions
+  .region('europe-west1')
+  .firestore.document('likes/{id}')
+  .onDelete((snapshot) => {
+    db.doc(`/nofifications/${snapshot.id}`)
+    .delete()
+    .then(() => {
+      return
+    })
+    .catch(err => {
+      console.error(err)
+      return
+    })
+  })
+
 exports.createNotificationOnComment = functions
   .region('europe-west1')
   .firestore.document('comments/{id}')
