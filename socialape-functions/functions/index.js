dc2d9be9863921f21ccbcb50679fae9d615ecd64
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-13 13:10:30 +0200

    created two new routes for getting user details and marking notifications as red

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -39,6 +39,8 @@ app.post('/login', login)
 app.post('/user/image', FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
 app.get('/user', FBAuth, getAuthenticatedUser)
+app.get('/user/:handle', getUserDetails)
+app.post('/notifictions', FBAuth, markNotificationsRead)
 
 exports.api = functions.https.onRequest(app)
 
