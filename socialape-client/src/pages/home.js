780ed1312ce309179ee92cd136e81c9754fbb2ef
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-14 08:59:49 +0200

    created Grid template for Home page

--- socialape-client/src/pages/home.js
+++ socialape-client/src/pages/home.js
@@ -4,6 +4,14 @@ import Grid from '@material-ui/core/Grid'
 export class Home extends React.Component {
   render() {
     return (
+      <Grid container>
+        <Grid item sm={8} xs={12}>
+          <p>Content...</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
     )
   }
 }
