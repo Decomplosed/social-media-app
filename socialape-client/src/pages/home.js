a2875cc2279d6848f9db0cbc9112c87d4363397f
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-14 09:11:15 +0200

    render body for now to check if the API works

--- socialape-client/src/pages/home.js
+++ socialape-client/src/pages/home.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import axios from 'axios'
 import Grid from '@material-ui/core/Grid'
 
 export class Home extends React.Component {
+  state = {
+    screams: null,
+  }
+
+  componentDidMount() {
+    axios
+      .get('/screams')
+      .then((res) => {
+        console.log(res.data)
+        this.setState({
+          screams: res.data,
+        })
+      })
+      .catch((err) => console.log(err))
+  }
+
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => <p>{scream.body}</p>)
+    ) : (
+      <p>Loading ...</p>
+    )
+
     return (
       <Grid container spacing={6}>
         <Grid item sm={8} xs={12}>
-          <p>Content...</p>
+          {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
           <p>Profile...</p>
