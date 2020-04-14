353406a522a77e68e2488ccd03aa56227eaaa37f
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-14 08:43:31 +0200

    wrapped ROutes inside container and styled it

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -14,11 +14,13 @@ function App() {
     <div className='App'>
       <Router>
         <Navbar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={Signup} />
-        </Switch>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+          </Switch>
+        </div>
       </Router>
     </div>
   )
