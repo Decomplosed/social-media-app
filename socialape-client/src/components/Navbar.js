29f1c3e2c99165efd76494b6344eac83cb5d8b9f
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-14 08:46:31 +0200

    added className to Toolbar

--- socialape-client/src/components/Navbar.js
+++ socialape-client/src/components/Navbar.js
@@ -8,7 +8,7 @@ export class Navbar extends Component {
   render() {
     return (
       <AppBar position='fixed'>
-        <Toolbar>
+        <Toolbar className='nav-container'>
           <Button color='inherit' component={Link} to='/login'>
             Login
           </Button>
