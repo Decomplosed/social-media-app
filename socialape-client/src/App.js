db69477319904598c351355e4aac2abc0e290cc3
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-14 08:55:29 +0200

    implemented theme

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -11,21 +11,40 @@ import Navbar from './components/Navbar'
 
 import './App.css'
 
-function App() {
-  return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  )
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff',
+    },
+  },
+})
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App
