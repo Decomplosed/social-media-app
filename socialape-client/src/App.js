86843ee48db3fe43869cbdce72dec57d4d6612ae
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-14 09:01:56 +0200

    fixed imports to resolve warnings in console

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 import Home from './pages/Home'
