62974ce225da19656e26bea7a450a2e12051f691
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-14 08:38:47 +0200

    imported couple of components from material ui

--- socialape-client/src/components/Navbar.js
+++ socialape-client/src/components/Navbar.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
 
 export class Navbar extends Component {
   render() {
-    return <div></div>
+    return (
+      <AppBar position='fixed'>
+        <Toolbar></Toolbar>
+      </AppBar>
+    )
   }
 }
 
