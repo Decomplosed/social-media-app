752ea45bd34ec3b5a1f02bdfe76b3e3e1471cac1
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-15 16:57:35 +0200

    import dayjs

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
+import { Link } from 'react-router-dom'
+import dayjs from 'dayjs'
 
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import { Link } from 'react-router-dom'
 
 const styles = {
   card: {
