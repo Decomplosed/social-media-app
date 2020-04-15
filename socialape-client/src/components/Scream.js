af857e246cbbe240d67e12bc6540512728efd37b
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-15 16:39:21 +0200

    changed Link import to resolve warnings

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -5,7 +5,7 @@ import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import Link from 'react-router-dom/Link'
+import { Link } from 'react-router-dom'
 
 const styles = {
   card: {
