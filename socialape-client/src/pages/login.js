fe78c69fa261e01d72fa3c2eb32aa47c69c4800f
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-15 17:11:20 +0200

    import monkey icon

--- socialape-client/src/pages/login.js
+++ socialape-client/src/pages/login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
+import AppIcon from '../images/icon.png'
 
 import Grid from '@material-ui/core/Grid'
 
