cc0cd34d9a5cd0ed3bad09b376a33c9080c47af9
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-15 16:16:22 +0200

    imported Link

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
+
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
 
 const styles = {
   card: {
