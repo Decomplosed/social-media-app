45874c4629372e52e1f469cbbe47f160eb95c24f
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-15 17:03:04 +0200

    initialised styles and extracted classes from props

--- socialape-client/src/pages/login.js
+++ socialape-client/src/pages/login.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 
+const styles = {}
+
 export class Login extends Component {
   render() {
+    const { classes } = this.props
+
     return (
       <div>
         <h1>Login page</h1>
