f0b4e7c2cc44440e06f75eb58db31ab2702c0346
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-16 16:49:25 +0200

    inset themeObject inside theme creator

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -6,27 +6,12 @@ import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-
 import Navbar from './components/Navbar'
 
+import themeObject from './utils/theme'
 import './App.css'
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff',
-    },
-  },
-})
+const theme = createMuiTheme(themeObject)
 
 class App extends React.Component {
   render() {
