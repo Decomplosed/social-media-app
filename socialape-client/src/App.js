203daede4066604d9c56dbd894bcde58631ded75
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-16 17:18:54 +0200

    import AuthRoute

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
+import AuthRoute from './utils/AuthRoute'
 
 import themeObject from './utils/theme'
 import './App.css'
