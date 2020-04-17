80ea400f1dbc55c128a70de8b4248a5754772015
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-17 16:29:18 +0200

    formatting fixes

--- socialape-client/src/redux/store.js
+++ socialape-client/src/redux/store.js
@@ -15,4 +15,11 @@ const reducers = combineReducers({
   UI: uiReducer,
 })
 
-const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(
+  reducers,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
