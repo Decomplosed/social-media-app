470488ba1d34c9e75c2984bd50cb2a723e1d1bc5
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-17 16:26:29 +0200

    created store

--- socialape-client/src/redux/store.js
+++ socialape-client/src/redux/store.js
@@ -14,3 +14,5 @@ const reducers = combineReducers({
   data: dataReducer,
   UI: uiReducer,
 })
+
+const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__()))
