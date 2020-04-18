be3cb6ba5674680abbfb3976275fd60b859395b3
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-18 23:00:29 +0200

    started creating switch statement for actions

--- socialape-client/src/redux/reducers/userReducer.js
+++ socialape-client/src/redux/reducers/userReducer.js
@@ -1,8 +1,25 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types'
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_AUTHENTICATED,
+} from '../types'
 
 const initialState = {
   authenticated: false,
   credentials: {},
   likes: [],
-  notifications: []
+  notifications: [],
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SET_AUTHENTICATED:
+      return {}
+    default:
+      return {
+        ...state,
+      }
+  }
 }
