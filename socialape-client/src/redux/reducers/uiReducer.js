09fa39ee83c0d2b05401cae891ccd001b10d51b5
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-18 23:15:28 +0200

    created a case for SET_ERRORS action

--- socialape-client/src/redux/reducers/uiReducer.js
+++ socialape-client/src/redux/reducers/uiReducer.js
@@ -7,6 +7,12 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case SET_ERRORS:
+      return {
+        ...state,
+        loading: false,
+        errors: action.payload,
+      }
     default:
       return state
   }
