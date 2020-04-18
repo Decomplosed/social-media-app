8e2722e4e287b2b6e70d7a7f59c5688101547e4a
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-18 22:59:12 +0200

    created initialState

--- socialape-client/src/redux/reducers/userReducer.js
+++ socialape-client/src/redux/reducers/userReducer.js
@@ -1,1 +1,8 @@
 import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types'
+
+const initialState = {
+  authenticated: false,
+  credentials: {},
+  likes: [],
+  notifications: []
+}
