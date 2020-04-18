bd1a0a5ded4f6dae7a39c7ad05be6c97ec5a9d87
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-18 23:14:43 +0200

    creaated switch case for actions

--- socialape-client/src/redux/reducers/uiReducer.js
+++ socialape-client/src/redux/reducers/uiReducer.js
@@ -4,3 +4,10 @@ const initialState = {
   loading: false,
   errors: null,
 }
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
