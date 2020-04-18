0c009c0a00e09657dd28b1301ce78e8a13e857a8
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-18 23:04:30 +0200

    imported connect

--- socialape-client/src/pages/login.js
+++ socialape-client/src/pages/login.js
@@ -11,6 +11,8 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
+import { connect } from 'react-redux'
+
 const styles = {
   form: {
     textAlign: 'center',
