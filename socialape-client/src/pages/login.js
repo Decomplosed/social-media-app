cdb4e7d23ce9644cb764089f56851a1d40b0a380
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-20 22:55:16 +0200

    imported withRouter

--- socialape-client/src/pages/login.js
+++ socialape-client/src/pages/login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 import AppIcon from '../images/icon.png'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
