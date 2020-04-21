a63061ab64a36238d06584d8e92dc041171cc6ae
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-21 09:08:49 +0200

    imported Button from material ui

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
 
+import Button from '@material-ui/core/Button'
+
 class Profile extends Component {
   render() {
     return <div></div>
