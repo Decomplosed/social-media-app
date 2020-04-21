7027bce2c8f06352a944e0b7b007d6d70d18df2c
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-21 09:48:56 +0200

    imported Paper and Link from material ui

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
 
 import Button from '@material-ui/core/Button'
+import MuiLink from '@material-ui/core/Link'
 
 import { connect } from 'react-redux'
+import { Paper } from '@material-ui/core'
 
 const styles = {
   paper: {
