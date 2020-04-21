d9d31697d68b7eeac767ebc154bc6e06f8f425fb
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-21 09:53:10 +0200

    imported Typography

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button'
 import MuiLink from '@material-ui/core/Link'
 
 import { connect } from 'react-redux'
-import { Paper } from '@material-ui/core'
+import { Paper, Typography } from '@material-ui/core'
 
 const styles = {
   paper: {
