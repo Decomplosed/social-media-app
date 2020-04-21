838bbba44f52e3aba4bbbb6c2516046bfb941e75
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-21 10:03:52 +0200

    added section for unauthenticated person

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import dayjs from 'dayjs'
 
 import Button from '@material-ui/core/Button'
@@ -72,41 +72,67 @@ class Profile extends Component {
       },
     } = this.props
 
-    let profileMarkup = !loading ? (authenticated ? (
-      <Paper className={classes.paper}>
-        <div className={classes.profile}>
-          <div className="profile-image">
-            <img src={imageUrl} alt="Prfile"/>
-          </div>
-          <hr />
-          <div className="profile-details">
-            <MuiLink component={Link} to={`/users/${handle}`} color='primary' variant='h5'>
-              @{handle}
-            </MuiLink>
+    let profileMarkup = !loading ? (
+      authenticated ? (
+        <Paper className={classes.paper}>
+          <div className={classes.profile}>
+            <div className='profile-image'>
+              <img src={imageUrl} alt='Prfile' />
+            </div>
             <hr />
-    {bio && <Typography variant='body2'>{bio}</Typography>}
-    <hr />
-    {location && (
-      <Fragment>
-        <LoactionOn color='primary' /> <span>{location}</span>
-        <hr />
-      </Fragment>
-    )}
-    {website && (
-      <Fragment>
-        <LinkIcon color='primary' />
-        <a href={website} target='_blank' rel='noopener noreferrer'>
-          {' '}{website}
-        </a>
-        <hr />
-      </Fragment>
-    )}
-    <CalendarToday color='primary' />
-    <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
+            <div className='profile-details'>
+              <MuiLink
+                component={Link}
+                to={`/users/${handle}`}
+                color='primary'
+                variant='h5'
+              >
+                @{handle}
+              </MuiLink>
+              <hr />
+              {bio && <Typography variant='body2'>{bio}</Typography>}
+              <hr />
+              {location && (
+                <Fragment>
+                  <LoactionOn color='primary' /> <span>{location}</span>
+                  <hr />
+                </Fragment>
+              )}
+              {website && (
+                <Fragment>
+                  <LinkIcon color='primary' />
+                  <a href={website} target='_blank' rel='noopener noreferrer'>
+                    {' '}
+                    {website}
+                  </a>
+                  <hr />
+                </Fragment>
+              )}
+              <CalendarToday color='primary' />
+              <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
+            </div>
           </div>
-        </div>
-      </Paper>
-    ) : ()) : (<p>Loading...</p>)
+        </Paper>
+      ) : (
+        <Paper className={classes.paper}>
+          <Typography variant='body2' align='center'>
+            No profile found, please login again
+          </Typography>
+          <div className={classes.buttons}>
+            <Button
+              variant='contained'
+              color='primary'
+              component={Link}
+              to='/login'
+            >
+              Login
+            </Button>
+          </div>
+        </Paper>
+      )
+    ) : (
+      <p>Loading...</p>
+    )
 
     return profileMarkup
   }
