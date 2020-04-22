e45a6ee45bacbe03080f0f179fe2e19c2e821cb1
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-22 16:21:36 +0200

    created base for EditDetails component

---
+++ socialape-client/src/components/EditDetails.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+class EditDetails extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default EditDetails
