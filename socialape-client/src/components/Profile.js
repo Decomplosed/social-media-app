78cc8901cedd1589ed6b35b1588326c05db83457
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-22 16:33:45 +0200

    implemented EditDetails to the template

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -153,6 +153,7 @@ class Profile extends Component {
                 <KeyboardReturn color='primary' />
               </IconButton>
             </Tooltip>
+            <EditDetails />
           </div>
         </Paper>
       ) : (
