8035721e867f2ead2c336d2ed16fc6b5931e95e5
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-24 22:55:58 +0200

    initialised switch

--- socialape-client/src/redux/reducers/dataReducer.js
+++ socialape-client/src/redux/reducers/dataReducer.js
@@ -5,3 +5,10 @@ const initialState = {
   scream: {},
   loading: false,
 }
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
