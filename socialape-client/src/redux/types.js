4b912ce8b9792ee15eec4f8eb3f311ff072c53e3
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-24 22:47:28 +0200

    added types of LIKE_SCREAM and UNLIKE_SCREAM

--- socialape-client/src/redux/types.js
+++ socialape-client/src/redux/types.js
@@ -9,3 +9,5 @@ export const CLEAR_ERRORS = 'CLEAR_ERRORS'
 
 export const SET_SCREAMS = 'SET_SCREAMS'
 export const SET_SCREAM = 'SET_SCREAM'
+export const LIKE_SCREAM = 'LIKE_SCREAM'
+export const UNLIKE_SCREAM = 'UNLIKE_SCREAM'
