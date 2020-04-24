35f9cfcdf8eb1b21f164b845d87c0cfb03db274b
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-24 22:14:36 +0200

    removed unused import

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -9,7 +9,6 @@ import UtilButton from '../utils/UtilButton'
 import Button from '@material-ui/core/Button'
 import MuiLink from '@material-ui/core/Link'
 import IconButton from '@material-ui/core/IconButton'
-import Tooltip from '@material-ui/core/Tooltip'
 
 import LoactionOn from '@material-ui/icons/LocationOn'
 import LinkIcon from '@material-ui/icons/Link'
