828bbe4c957e2fd71e6ee5d763a404f6ea19ffc9
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-25 22:29:10 +0200

    imported PropTypes

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -3,6 +3,7 @@ import withStyles from '@material-ui/core/styles/withStyles'
 import { Link } from 'react-router-dom'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import PropTypes from 'prop-types'
 
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
