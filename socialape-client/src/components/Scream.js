5fe771c063247dad704fa7f6659b294f449ac7e6
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-26 09:54:06 +0200

    implemented deleteButton inside the template

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -111,6 +111,7 @@ export class Scream extends React.Component {
           >
             {userHandle}
           </Typography>
+          {deleteButton}
           <Typography variant='body2' color='textSecondary'>
             {dayjs(createdAt).fromNow()}
           </Typography>
