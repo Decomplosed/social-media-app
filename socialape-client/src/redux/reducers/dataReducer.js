cc5b6379bb41fe06b0cfa8f6251ae0eebd3808a7
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-26 09:45:49 +0200

    changed the way of LIKE_SCREAM and UNLIKE_SCREAM cases

--- socialape-client/src/redux/reducers/dataReducer.js
+++ socialape-client/src/redux/reducers/dataReducer.js
@@ -24,7 +24,10 @@ export default function (state = initialState, action) {
       let index = state.screams.findIndex(
         (scream) => scream.screamId === action.payload.screamId
       )
-      state.scream[index] = action.payload
+      state.screams[index] = action.payload
+      if (state.scream.screamId === action.payload.screamId) {
+        state.scream = action.payload
+      }
       return {
         ...state,
       }
