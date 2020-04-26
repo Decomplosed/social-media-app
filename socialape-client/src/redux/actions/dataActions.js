b9252e69d86c71d9c92104331f75cd26ddd0d990
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-26 09:45:19 +0200

    formatting changes inside dataActions

--- socialape-client/src/redux/actions/dataActions.js
+++ socialape-client/src/redux/actions/dataActions.js
@@ -14,13 +14,17 @@ export const getScreams = () => (dispatch) => {
 export const likeScream = (screamId) => (dispatch) => {
   axios
     .get(`/scream/${screamId}/like`)
-    .then((res) => dispatch({ type: LIKE_SCREAM, payload: res.data }))
+    .then((res) => {
+      dispatch({ type: LIKE_SCREAM, payload: res.data })
+    })
     .catch((err) => console.log(err))
 }
 
 export const unlikeScream = (screamId) => (dispatch) => {
   axios
     .get(`/scream/${screamId}/unlike`)
-    .then((res) => dispatch({ type: UNLIKE_SCREAM, payload: res.data }))
+    .then((res) => {
+      dispatch({ type: UNLIKE_SCREAM, payload: res.data })
+    })
     .catch((err) => console.log(err))
 }
