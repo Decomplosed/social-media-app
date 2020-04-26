d61d2c5630227e5c9d497c108a350ca08363ad74
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-26 09:55:42 +0200

    imported MUI components

--- socialape-client/src/components/DeleteScream.js
+++ socialape-client/src/components/DeleteScream.js
@@ -3,7 +3,11 @@ import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 import UtilButton from '../utils/UtilButton'
 
-
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogActions from '@material-ui/core/DialogActions'
+import DeleteOutline from '@material-ui/icons/DeleteOutline'
 
 class DeleteScream extends Component {
   render() {
