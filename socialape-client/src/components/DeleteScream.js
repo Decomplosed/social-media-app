0ac92a0669604171f2e70a3472f6b95b1a7a0113
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-26 09:53:30 +0200

    imported withStyles and proptypes

--- socialape-client/src/components/DeleteScream.js
+++ socialape-client/src/components/DeleteScream.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+import PropTypes from 'prop-types'
+import UtilButton from '../utils/UtilButton'
+
+
 
 class DeleteScream extends Component {
   render() {
-    return (
-      <div>
-
-      </div>
-    )
+    return <div></div>
   }
 }
 
