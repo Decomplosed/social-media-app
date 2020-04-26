6c671b227cfadd0f0226d065867f85dd960f193d
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-26 09:58:57 +0200

    imported DELETE_SCREAM type to the dataActions

--- socialape-client/src/redux/actions/dataActions.js
+++ socialape-client/src/redux/actions/dataActions.js
@@ -1,4 +1,4 @@
-import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM } from '../types'
+import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM, DELETE_SCREAM } from '../types'
 import axios from 'axios'
 
 export const getScreams = () => (dispatch) => {
