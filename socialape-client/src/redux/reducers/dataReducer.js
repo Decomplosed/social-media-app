e2ebab7ea6f6362d5ecf8f3c8dbe9be5326e866e
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-27 22:05:15 +0200

    changed using findIndex with simple filter method

--- socialape-client/src/redux/reducers/dataReducer.js
+++ socialape-client/src/redux/reducers/dataReducer.js
@@ -39,12 +39,11 @@ export default function (state = initialState, action) {
         ...state,
       }
     case DELETE_SCREAM:
-      index = state.screams.findIndex(
-        (scream) => scream.screamId === action.payload
-      )
-      state.screams.splice(index, 1)
       return {
         ...state,
+        screams: state.screams.filter(
+          (scream) => scream.screamId !== action.payload
+        ),
       }
     case POST_SCREAM:
       return {
