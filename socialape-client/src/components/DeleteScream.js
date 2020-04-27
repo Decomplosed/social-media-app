fda9e88ab2e3fb387698973dbec49371c958f2e1
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-27 21:51:12 +0200

    better positioning of the delete scream button

--- socialape-client/src/components/DeleteScream.js
+++ socialape-client/src/components/DeleteScream.js
@@ -15,8 +15,8 @@ import { deleteScream } from '../redux/actions/dataActions'
 const styles = {
   deleteButton: {
     position: 'absolute',
-    left: '90%',
-    top: '10%',
+    left: '92%',
+    top: '5%',
   },
 }
 
