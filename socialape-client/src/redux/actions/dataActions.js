5504df835bf42612b47e388833a84dff1836b21b
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-27 17:31:44 +0200

    created postScream action

--- socialape-client/src/redux/actions/dataActions.js
+++ socialape-client/src/redux/actions/dataActions.js
@@ -1,4 +1,11 @@
-import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM, DELETE_SCREAM } from '../types'
+import {
+  SET_SCREAMS,
+  LOADING_DATA,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  DELETE_SCREAM,
+  LOADING_UI,
+} from '../types'
 import axios from 'axios'
 
 export const getScreams = () => (dispatch) => {
@@ -9,6 +16,23 @@ export const getScreams = () => (dispatch) => {
       dispatch({ type: SET_SCREAMS, payload: res.data })
     })
     .catch((err) => dispatch({ type: SET_SCREAMS, payload: [] }))
+}
+
+export const postScream = (newScream) => (dispatch) => {
+  dispatch({ type: LOADING_UI })
+
+  axios
+    .post('/scream', newScream)
+    .then((res) => {
+      dispatch({
+        type: POST_SCREAM,
+        payload: res.data,
+      })
+      dispatch({ CLEAR_ERRORS })
+    })
+    .catch((err) => {
+      dispatch({ type: SET_ERRORS, payload: err.response.data })
+    })
 }
 
 export const likeScream = (screamId) => (dispatch) => {
