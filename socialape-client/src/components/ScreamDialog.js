8dac175d5ed4852598dfaa779564883aa02137e9
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-28 08:44:20 +0200

    imported Link

--- socialape-client/src/components/ScreamDialog.js
+++ socialape-client/src/components/ScreamDialog.js
@@ -3,6 +3,7 @@ import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 import UtilButton from '../utils/UtilButton'
 import dayjs from 'dayjs'
+import { Link } from 'react-router-dom'
 
 class ScreamDialog extends Component {
   render() {
