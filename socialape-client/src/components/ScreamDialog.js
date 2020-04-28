e3927eeae1ae4abf6ab89900a3e7a7c0b28e6f9a
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-28 08:43:35 +0200

    created a base template

---
+++ socialape-client/src/components/ScreamDialog.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+import PropTypes from 'prop-types'
+import UtilButton from '../utils/UtilButton'
+
+class ScreamDialog extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default ScreamDialog
