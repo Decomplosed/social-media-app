321f5471d088d5e6f482f7b316188b0418c3428c
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-28 08:47:27 +0200

    imported Typography

--- socialape-client/src/components/ScreamDialog.js
+++ socialape-client/src/components/ScreamDialog.js
@@ -10,6 +10,7 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
 import CloseIcon from '@material-ui/icons/Close'
 
