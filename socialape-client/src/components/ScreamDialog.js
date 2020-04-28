94c3a13b9f70dc585217abb7c0763ba9d4bf8bcc
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-28 08:46:47 +0200

    imported Grid from MUI

--- socialape-client/src/components/ScreamDialog.js
+++ socialape-client/src/components/ScreamDialog.js
@@ -5,14 +5,11 @@ import UtilButton from '../utils/UtilButton'
 import dayjs from 'dayjs'
 import { Link } from 'react-router-dom'
 
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
-import DeleteOutline from '@material-ui/icons/DeleteOutline'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import Grid from '@material-ui/core/Grid'
 
 import CloseIcon from '@material-ui/icons/Close'
 
