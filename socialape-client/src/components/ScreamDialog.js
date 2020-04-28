065fa028d6ba614588d3dc9d00f1b781147ee4bc
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-28 08:43:59 +0200

    imported dayjs

--- socialape-client/src/components/ScreamDialog.js
+++ socialape-client/src/components/ScreamDialog.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 import UtilButton from '../utils/UtilButton'
+import dayjs from 'dayjs'
 
 class ScreamDialog extends Component {
   render() {
