e8ed1812e57b9ab43da3592be7e4ae13b837535a
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-28 08:41:35 +0200

    imported ScreamDialog

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -6,6 +6,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import PropTypes from 'prop-types'
 import UtilButton from '../utils/UtilButton'
 import DeleteScream from './DeleteScream'
+import ScreamDialog from './ScreamDialog'
 
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
