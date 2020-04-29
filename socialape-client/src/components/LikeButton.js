c346a427f2ec8b401f4fd06d85a4d70c52072256
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-29 16:01:12 +0200

    created separated LikeButton component

---
+++ socialape-client/src/components/LikeButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class LikeButton extends Component {
+  render() {
+    const likeButton = !authenticated ? (
+      <UtilButton tip='Like'>
+        <Link to='/login'>
+          <FavoriteBorder colot='primary' />
+        </Link>
+      </UtilButton>
+    ) : this.likedScream() ? (
+      <UtilButton tip='Undo Like' onClick={this.unlikeScream}>
+        <FavoriteIcon color='primary' />
+      </UtilButton>
+    ) : (
+      <UtilButton tip='Like' onClick={this.likeScream}>
+        <FavoriteBorder color='primary' />
+      </UtilButton>
+    )
+
+    return likeButton
+  }
+}
+
+export default LikeButton
