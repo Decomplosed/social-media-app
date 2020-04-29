fcc7f7bdd69ee2efe98e081b37260507d0002def
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-29 15:01:41 +0200

    created a case of SET_SCREAM

--- socialape-client/src/redux/reducers/dataReducer.js
+++ socialape-client/src/redux/reducers/dataReducer.js
@@ -28,6 +28,11 @@ export default function (state = initialState, action) {
         screams: action.payload,
         loading: false,
       }
+    case SET_SCREAM:
+      return {
+        ...state,
+        scream: action.payload,
+      }
     case LIKE_SCREAM:
     case UNLIKE_SCREAM:
       let index = state.screams.findIndex(
