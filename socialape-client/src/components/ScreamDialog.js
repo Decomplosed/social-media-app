ab4341ccda2aa1d4e0fe1e6c6d36f5b872b378df
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-29 16:33:25 +0200

    imported LikeButton

--- socialape-client/src/components/ScreamDialog.js
+++ socialape-client/src/components/ScreamDialog.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import UtilButton from '../utils/UtilButton'
 import dayjs from 'dayjs'
 import { Link } from 'react-router-dom'
+import LikeButton from './LikeButton'
 
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
