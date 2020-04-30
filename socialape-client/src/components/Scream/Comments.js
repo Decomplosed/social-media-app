d1a16d0377a52449feb0cadc913ab00acedcceb4
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-30 09:09:44 +0200

    import Link and fix the import of Typography

--- socialape-client/src/components/Scream/Comments.js
+++ socialape-client/src/components/Scream/Comments.js
@@ -1,9 +1,10 @@
 import React, { Component, Fragment } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 import Grid from '@material-ui/core/Grid'
-import Typograpghy from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography'
 
 const styles = (theme) => ({
   ...theme.globalStyles,
