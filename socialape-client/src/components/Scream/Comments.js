d1f65c8f82715c53654e6c96801bf3a40be428e9
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-30 09:16:28 +0200

    added styles for commentImage

--- socialape-client/src/components/Scream/Comments.js
+++ socialape-client/src/components/Scream/Comments.js
@@ -9,6 +9,12 @@ import Typography from '@material-ui/core/Typography'
 
 const styles = (theme) => ({
   ...theme.globalStyles,
+  commentImage: {
+    maxWidth: '100%',
+    height: 100,
+    objectFit: 'cover',
+    borderRadius: '50%',
+  },
 })
 
 export class Comments extends Component {
