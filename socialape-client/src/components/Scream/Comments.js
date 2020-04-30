6ddb453147a5e1154adb8f29af795156c8b094bb
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-30 09:12:13 +0200

    imported dayjs

--- socialape-client/src/components/Scream/Comments.js
+++ socialape-client/src/components/Scream/Comments.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import dayjs from 'dayjs'
 
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
