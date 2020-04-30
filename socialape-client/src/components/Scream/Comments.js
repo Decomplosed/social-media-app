494fe578a460e3a7a8908e9e2065762daf7fb65b
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-30 08:59:05 +0200

    imported Grid and Typography

--- socialape-client/src/components/Scream/Comments.js
+++ socialape-client/src/components/Scream/Comments.js
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 
+import Grid from '@material-ui/core/Grid'
+import Typograpghy from '@material-ui/core/Typography'
+
+
 export class Comments extends Component {
   render() {
     return <div></div>
