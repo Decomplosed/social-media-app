56fb8a4fa17ea47cbd7fe5cc8e59d50702a5ef1a
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-01 16:26:28 +0200

    imported Grid from MUI

--- socialape-client/src/pages/User.js
+++ socialape-client/src/pages/User.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
-
 import Scream from '../components/Scream/Scream'
+import Grid from '@material-ui/core/Grid'
+
 
 class User extends Component {
   render() {
