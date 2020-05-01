8da627ff7c9f6d55bfe13d3cf6eb80a078639daa
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-01 16:21:24 +0200

    imported Scream component

--- socialape-client/src/pages/User.js
+++ socialape-client/src/pages/User.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 
+import Scream from '../components/Scream/Scream'
+
 class User extends Component {
   render() {
     return <div></div>
