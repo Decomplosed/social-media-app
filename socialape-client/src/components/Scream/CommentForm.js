df37b8b850e71c1cd3cb89861aaf644d4c5c21ed
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-01 15:35:11 +0200

    imported Fragment, withStyles and PropTypes

--- socialape-client/src/components/Scream/CommentForm.js
+++ socialape-client/src/components/Scream/CommentForm.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+import PropTypes from 'prop-types'
 
 class CommentForm extends Component {
   render() {
