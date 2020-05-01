d294aedb3e8c0233f9950680ab91e681b90b5ca7
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-01 16:27:24 +0200

    imported connect and getUserData action

--- socialape-client/src/pages/User.js
+++ socialape-client/src/pages/User.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 import Scream from '../components/Scream/Scream'
 import Grid from '@material-ui/core/Grid'
 
+import { connect } from 'react-redux'
+import { getUserData } from '../redux/actions/dataActions'
 
 class User extends Component {
   render() {
