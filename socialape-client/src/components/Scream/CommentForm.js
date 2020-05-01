67de568cdaba9f139a8c75c87f47bffae89e7344
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-01 15:35:53 +0200

    imported MUI components

--- socialape-client/src/components/Scream/CommentForm.js
+++ socialape-client/src/components/Scream/CommentForm.js
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+
 class CommentForm extends Component {
   render() {
     return <div></div>
