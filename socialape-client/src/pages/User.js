f1d4be160c34284c9dc1d9a72db5f06b1aaf6835
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-02 22:49:05 +0200

    call getUserData with handle received from params

--- socialape-client/src/pages/User.js
+++ socialape-client/src/pages/User.js
@@ -10,6 +10,8 @@ import { getUserData } from '../redux/actions/dataActions'
 class User extends Component {
   componentDidMount() {
     const handle = this.props.match.params.handle
+
+    this.props.getUserData(handle)
   }
 
   render() {
