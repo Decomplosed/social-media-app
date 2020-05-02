7518989655c794bbcd97c47e9bbbfe211ff554b0
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-02 23:02:46 +0200

    imported dayjs

--- socialape-client/src/components/Profile/StaticProfile.js
+++ socialape-client/src/components/Profile/StaticProfile.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
+import dayjs from 'dayjs'
 
 export class StaticProfile extends Component {
   render() {
