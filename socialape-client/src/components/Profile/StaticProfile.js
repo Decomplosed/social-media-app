98a1811c7954450f41ddb104b5d6695fad501ea3
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-02 23:03:26 +0200

    imported MUI component

--- socialape-client/src/components/Profile/StaticProfile.js
+++ socialape-client/src/components/Profile/StaticProfile.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
 import dayjs from 'dayjs'
 
+import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
 export class StaticProfile extends Component {
   render() {
     return <div></div>
