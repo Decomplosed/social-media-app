3c29e0f1de67cfe56e22ab11279009183b44489e
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-02 23:10:55 +0200

    imported Link

--- socialape-client/src/components/Profile/StaticProfile.js
+++ socialape-client/src/components/Profile/StaticProfile.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
 import dayjs from 'dayjs'
+import { Link } from 'react-router-dom'
 
-import Link from '@material-ui/core/Link'
+import MuiLink from '@material-ui/core/Link'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 
