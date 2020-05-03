a2d7bc3f15fda4de03f81485c052ca3b4ba97e45
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-03 23:33:12 +0200

    added openDialog to proptypes

--- socialape-client/src/components/Scream/Scream.js
+++ socialape-client/src/components/Scream/Scream.js
@@ -104,6 +104,7 @@ Scream.propTypes = {
   user: PropTypes.object.isRequired,
   scream: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
+  openDialog: PropTypes.bool,
 }
 
 const mapStateToProps = (state) => ({
