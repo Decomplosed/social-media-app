a2af0dc1cd7f9bea84cfb8957bf03e24740ce879
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-03 23:41:19 +0200

    added missing history

--- socialape-client/src/components/Scream/ScreamDialog.js
+++ socialape-client/src/components/Scream/ScreamDialog.js
@@ -65,7 +65,7 @@ class ScreamDialog extends Component {
     const { userHandle, screamId } = this.props
     const newPath = `/users/${userHandle}/scream/${screamId}`
 
-    window.pushState(null, null, newPath)
+    window.hitory.pushState(null, null, newPath)
 
     this.setState({ open: true })
     this.props.getScream(this.props.screamId)
