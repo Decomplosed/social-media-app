4121918aba1589b4042e0e6bf7cbeb12ae057426
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-05 15:15:21 +0200

    imported Menu from MUI

--- socialape-client/src/components/Layout/Notifications.js
+++ socialape-client/src/components/Layout/Notifications.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import withStyles from '@material-ui/core/styles/withStyles'
 import { Link } from 'react-router-dom'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import PropTypes from 'prop-types'
+
+import Menu from '@material-ui/core/Menu'
 
 export class Notifications extends Component {
   render() {
