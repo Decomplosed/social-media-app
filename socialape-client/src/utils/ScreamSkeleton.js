9354ed6e277332e9a73488b17ccefbaa06ad15af
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-05 16:37:49 +0200

    imported withStyles

--- socialape-client/src/utils/ScreamSkeleton.js
+++ socialape-client/src/utils/ScreamSkeleton.js
@@ -5,3 +5,5 @@ import PropTypes from 'prop-types'
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
+
+import withStyles from '@material-ui/core/styles/withStyles'
