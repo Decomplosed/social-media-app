d6f23b98d05afb7238ab6a2deb10a10f85a3ebee
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-05 16:37:20 +0200

    imported MUI stuff

--- socialape-client/src/utils/ScreamSkeleton.js
+++ socialape-client/src/utils/ScreamSkeleton.js
@@ -1,3 +1,7 @@
 import React, { Fragment } from 'react'
 import NoImg from '../images/no-img.png'
 import PropTypes from 'prop-types'
+
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
