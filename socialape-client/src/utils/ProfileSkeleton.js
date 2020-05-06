48f9224361b86071d8c073ffaa7037d51105a4cd
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-06 17:07:32 +0200

    imported all icons from MUI

--- socialape-client/src/utils/ProfileSkeleton.js
+++ socialape-client/src/utils/ProfileSkeleton.js
@@ -3,6 +3,12 @@ import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
 import NoImg from '../images/no-img.png'
 
+import LocationOn from '@material-ui/icons/LocationOn'
+import LinkIcon from '@material-ui/icons/Link'
+import CalendarToday from '@material-ui/icons/CalendarToday'
+import EditIcon from '@material-ui/icons/Edit'
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn'
+
 const styles = (theme) => ({
   ...theme.globalStyles,
 })
