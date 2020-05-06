050ed58f177e58dced0f28c13d7e74ed3fd84049
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-06 17:06:53 +0200

    imported default image

--- socialape-client/src/utils/ProfileSkeleton.js
+++ socialape-client/src/utils/ProfileSkeleton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import PropTypes from 'prop-types'
-
+import NoImg from '../images/no-img.png'
 
 const styles = (theme) => ({
   ...theme.globalStyles,
